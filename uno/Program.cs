534f4b9c883e145c0bb479e5c84abeda1c52ddbe
Simon Rosén <simon.rosen@elev.ga.ntig.se>
2023-11-09 17:22:55 +0100

    n

--- uno/Program.cs
+++ uno/Program.cs
@@ -1,12 +1,62 @@
-﻿using Raylib_cs;
-using System.Numerics;
+﻿Random random = new Random();
+
+int numOfCards = 7;
+int whosTurn = 1; //Whos turn it is  1 = player1, 2 = player2 etc...
+
+string[] scenes = { "start", "game", "end" };
+int currentScene = 0;
+
+string[] colors = { "Red", "Green", "Blue", "Yellow" };
+List<Card> cards = new List<Card>();
 
 
-while (!Raylib.WindowShouldClose()){
-    Raylib.BeginDrawing();
+void DrawCard()
+{
+    Card card = new Card();
 
-    
+    card.color = random.Next(1, 4);
+    card.number = random.Next(0, 9);
+
+    cards.Add(card);
+}
 
 
-    Raylib.EndDrawing();
+void DisplayCards()
+{
+    for (int i = 0; i < cards.Count(); i++)
+    {
+        Console.WriteLine($"\ncard {i + 1} = {colors[cards[i].color]} {cards[i].number}");
+    }
+}
+
+
+while (numOfCards > 0)
+{
+    if (currentScene == 0)
+    {
+        Console.WriteLine("---UNO---");
+        Console.WriteLine("Press enter to start");
+        Console.ReadLine();
+
+        //Give 7 cards at start
+        for (int i = 0; i < numOfCards; i++)
+        {
+            DrawCard();
+        }
+
+        currentScene = 1;
+    }
+    else if (currentScene == 1)
+    {
+        if (whosTurn == 1){
+            DisplayCards();
+        }
+    }
+}
+
+
+public class Card
+{
+    public int number = 0;
+    public int color = 0;
 }
